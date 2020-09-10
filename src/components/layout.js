09876fbd17d88c6bbe644dfd4d1b808825fc7a72
Angie810 <angie.granberg@gmail.com>
2020-09-10 14:07:24 +0200

    Initial Commit

---
+++ src/components/layout.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+
+import { rhythm, scale } from "../utils/typography"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1>
+        <Link
+          
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+      
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          to={`/`}
+        >
+          {title}
+        </Link>
+        
+      </h3>
+    )
+  }
+  return (
+    <div>
+        <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
